633bd2c828b07a24f77523350773f82919bce2a2
OverEuro <yangyijun94@163.com>
2019-08-15 17:15:00 +0800

    Add files via upload

--- src/training.py
+++ src/training.py
@@ -9,7 +9,8 @@ class con_bandit():
     '''stationary bandit problem'''
     def __init__(self):
         self.state = 0
-        self.bandits = np.array([[.9,.9,.6,-5],[.7,-5,1,.8],[-5,.9,.6,.8]])
+        self.bandits = np.array([[.9,.9,.6,-5],[.7,-5,1,.8],[-5,.9,.6,.8],
+                                 [.5,.6,1,-5],[.4,.5,-5,.8],[.4,-5,.7,.8]])
         self.num_bandits = self.bandits.shape[0]
         self.num_actions = self.bandits.shape[1]
         
@@ -34,9 +35,7 @@ class agent(nn.Module):
     def __init__(self, input_s, output_s):
         super(agent, self).__init__()
         self.net = nn.Sequential(
-                    nn.Linear(input_s, 2*input_s),
-                    nn.Sigmoid(),
-                    nn.Linear(2*input_s, output_s),
+                    nn.Linear(input_s, output_s),
                     nn.Sigmoid())
         
     def forward(self, x):
@@ -60,13 +59,14 @@ loss_fun = loss()
 one_hot = F.one_hot(th.arange(0, env.num_bandits)).float().cuda()
 epochs = 30000
 e = 0.5 # epsilon for exploration
-#optimizer = th.optim.SGD(learner.parameters(), lr = 0.01, 
-#                         momentum=0.9)
-optimizer = th.optim.RMSprop(learner.parameters(), lr = 0.001, momentum=0.8)
-lr_sche = th.optim.lr_scheduler.MultiStepLR(optimizer, [6000, 8000], gamma=0.1)
+#optimizer = th.optim.SGD(learner.parameters(), lr = 0.001, momentum=0.9)
+#optimizer = th.optim.RMSprop(learner.parameters(), lr = 0.001, momentum=0.8)
+optimizer = th.optim.Adam(learner.parameters(), lr = 0.001)
+#lr_sche = th.optim.lr_scheduler.MultiStepLR(optimizer, [6000, 8000], gamma=0.1)
 total_reward = np.zeros([env.num_bandits,env.num_actions])
 
-loss_cur = []
+rew_curs = np.empty((epochs, env.num_bandits))
+sum_cur = []
 for epoch in range(epochs):
     
     s = env.getState() #Get a state from the environment.
@@ -80,26 +80,24 @@ for epoch in range(epochs):
         action = th.argmax(pro_list)
         
     reward = env.pullArm(action) #reward for taking an action given a bandit.
-    
+
     #Update the network.
     loss = loss_fun(pro_list, action, reward)
-    loss_cur.append(loss.item())
+#    loss_cur.append(loss.item())
 
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
     
-    #Update lr
-    lr_sche.step()
+#    lr_sche.step()
     
     total_reward[s, action] += reward
     if epoch % 500 == 0:
         print("Mean reward for each of the " + str(env.num_bandits) + 
               " bandits: " + str(np.mean(total_reward,axis=1)))
-    
+    rew_curs[epoch, :] = np.mean(total_reward,axis=1)
 
 output = learner.forward(one_hot).cpu().detach().numpy()
-output = np.array(output)
 for a in range(env.num_bandits):
     print("The agent thinks action " + str(np.argmax(output[a,:])+1) + " for bandit " + str(a+1) + " is the most promising")
     if np.argmax(output[a,:]) == np.argmin(env.bandits[a,:]):
@@ -108,7 +106,10 @@ for a in range(env.num_bandits):
         print("and it was wrong!")
 
 plt.figure()
-plt.plot(loss_cur)
+for i in range(env.num_bandits):
+    plt.plot(rew_curs[:, i], label='No.'+str(i+1))
+plt.legend()
+#plt.savefig('res.png',dpi=600)
 plt.show()
 
 
